8d5af61d6aa277f189edb34743acba1a5b381b55
jgroc-de <jgroc-de@student-42.fr>
2019-03-30 18:17:49 +0100

    correction pour 42

--- GNL/test.sh
+++ GNL/test.sh
@@ -6,29 +6,47 @@
 #    By: jgroc-de <marvin@42.fr>                    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2018/12/25 14:19:22 by jgroc-de          #+#    #+#              #
-#    Updated: 2018/12/25 14:23:01 by jgroc-de         ###   ########.fr        #
+#    Updated: 2019/03/30 17:27:12 by jgroc-de         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
+#!/bin/bash
+
 FILES="$@"
-make
 if [ $# -eq 0 ]; then
-	echo "Usage: sh $0 <test file 1> <test file 2> <...>"
-	echo "	ex: sh $0 test*"
+	echo "norminette"
+	norminette **/**.[ch] | grep "Error"
+	echo "fin norminette"
+	echo "make"
+	make
+	echo "fin make"
+	echo "Usage: $0 <test file 1> <test file 2> <...>"
+	echo "	ex: $0 test*"
 else
+	echo "test simple (ctrl+d pour finir)"
+	./test_gnl
+	echo "fin test simple"
+	i=0
 	for f in $FILES
 	do
 		echo "** début test de $f"
 		cat $f > tmpcat
-		$0 $f > tmpgnl
+		./test_gnl $f > tmpgnl
 		diff tmpcat tmpgnl
-		echo "\t*test leaks de $f"
-		valgrind $0 $f | grep lost
+		#echo "\t*test leaks de $f"
+		#valgrind ./test_gnl $f | grep lost
 		#valgrind --leak-check=full $0 $f | grep lost
 		echo "**fin test de $f"
+		((i++))
 	done
-	echo "null"
-	$0 $f /dev/null
-	echo "random"
-	$0 $f /dev/random
+	if [ $i -gt 4 ]; then
+		echo "** multi fd: début test de $3 $4"
+		./test_gnl $3 $4
+	fi
+	echo "** début test de null"
+	./test_gnl $f /dev/null
+	echo "** fin test de null"
+	echo "début test de random"
+	./test_gnl $f /dev/random
+	echo "** fin test de null"
 fi
